17a2da9bdbd53a58e56c5ac980a63d5cb36d9567
Ian McClaflin <ian.mcclaflin@outlook.com>
2022-02-02 06:34:39 -0600

    Added student

---
+++ ClassLibrary1/Student.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class Student
+    {
+        private string _fName;
+        private string _lName;
+        private string _iD;
+        private float _gPa;
+
+        public string FirstName
+        {
+            get { return _fName; }
+            set { _fName = value; }
+        }
+
+        public string LastName
+        {
+            get { return _lName; }
+            set { _lName = value; }
+        }
+
+        public string IdNumber
+        {
+            get { return _iD; }
+            set { _iD = value; }
+        }
+
+        public float GPA
+        {
+            get { return _gPa; }
+            set { _gPa = value; }
+        }
+
+        public Student(string firstname, string lastname, string idnumber, float gpa)
+        {
+            FirstName = firstname;
+            LastName = lastname;
+            IdNumber = idnumber;
+            GPA = gpa;
+        }
+
+        public Student() { }
+
+        public override string ToString()
+        {
+            //return base.ToString();
+            return string.Format("First Name {0}\n" +
+                "Last Name {1}\n" +
+                "idnumber {2}\n" +
+                "gpa {3}\n",
+                FirstName,
+                LastName,
+                IdNumber,
+                GPA);
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ClassLibrary1;
 
 namespace ConsoleApp1
 {
@@ -10,6 +11,22 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+
+            Console.WriteLine("This is my CSF2 Week Homework Packet");
+            Console.Title = "CSF2 Week Homework Packet";
+
+            Student stu1 = new Student();
+            stu1.FirstName = "Steve";
+            stu1.LastName = "Stevenson";
+            stu1.IdNumber = "123";
+            stu1.GPA = 3.4f;
+
+            Console.WriteLine("Students firstname {0}\n" +
+                "Students lastname {1}\n" +
+                "Students idnumber {2}\n" +
+                "Students GPA {3}", stu1.FirstName, stu1.LastName, stu1.IdNumber, stu1.GPA);
+
+
         }
     }
 }
